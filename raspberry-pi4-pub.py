334c2056cc3605e9d0f6b6288c383223aa10aa73
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2021-05-11 18:27:09 +0530

    Update raspberry-pi4-pub.py

--- raspberry-pi4-pub.py
+++ raspberry-pi4-pub.py
@@ -1,13 +1,9 @@
 import paho.mqtt.client as mqtt
 import time
 
-def on_message(client, userdata, message):
-    print (str(message.payload.decode("utf-8")))
+
     
-    
-    
-    
-def sub():
+def pub():
     mqttBroker ="test.mosquitto.org"
     client = mqtt.Client("raspberry pi 40")
     client.connect(mqttBroker)
