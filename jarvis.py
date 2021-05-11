6d2ba0a1398a3644ac02e16d3b8232ad1f1e1bb1
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2021-05-11 17:54:19 +0530

    Create jarvis.py

---
+++ jarvis.py
@@ -0,0 +1,10 @@
+import speech_recognition as sr
+
+def audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+    data=r.recognize_google(audio)
+    print(data)
+
